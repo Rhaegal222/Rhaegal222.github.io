e7582dca182bff6b45b89df6bd4c2544f1134eb1
Rhaegal222 <rhaegal222@gmail.com>
2024-04-19 15:39:11 +0200

    Inizializzato il progetto SpringBoot PersonalWebSite

---
+++ website/src/test/java/io/github/rhaegal222/website/PersonalWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.rhaegal222.website;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
